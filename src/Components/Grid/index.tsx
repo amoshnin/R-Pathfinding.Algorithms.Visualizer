282c1b693cb83138a37dcd9c60401cfd53584b05
Artem <swift.uix@gmail.com>
2021-08-29 16:47:45 +0300

    d

--- src/Components/Grid/index.tsx
+++ src/Components/Grid/index.tsx
@@ -2,14 +2,14 @@ import React, {Component} from 'react';
 import Node from './Node/Node';
 import {dijkstra, getNodesInShortestPathOrder} from '../algorithms/dijkstra';
 
-import './PathfindingVisualizer.css';
+import './Grid.css';
 
 const START_NODE_ROW = 10;
 const START_NODE_COL = 15;
 const FINISH_NODE_ROW = 10;
 const FINISH_NODE_COL = 35;
 
-export default class PathfindingVisualizer extends Component {
+export default class Grid extends Component {
   constructor() {
     super();
     this.state = {
